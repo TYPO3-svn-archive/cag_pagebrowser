b4da747d81b09eabed399f445df63789bcb3cf61
schrade <schrade@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-08-15 12:12:27 +0000

    Version 2.0.0 released in TER.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cag_pagebrowser/trunk@65416 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "cag_pagebrowser".
 #
-# Auto generated 05-07-2011 23:00
+# Auto generated 15-08-2012 14:11
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Quickly insert paginations (prev/next menus) in sections of your website',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '1.2.2',
+	'version' => '2.0.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'state' => 'stable',
 	'uploadfolder' => 0,
 	'createDirs' => '',
-	'modify_tables' => 'pages',
+	'modify_tables' => '',
 	'clearcacheonload' => 0,
 	'lockType' => '',
 	'author' => 'Torsten Schrade',
@@ -35,14 +35,14 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '3.0.0-0.0.0',
-			'typo3' => '3.5.0-4.8.0',
+			'typo3' => '3.5.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:20:{s:13:"CHANGELOG.txt";s:4:"f6f8";s:8:"TODO.txt";s:4:"d41d";s:21:"ext_conf_template.txt";s:4:"30ee";s:12:"ext_icon.gif";s:4:"aea2";s:17:"ext_localconf.php";s:4:"7d5e";s:14:"ext_tables.php";s:4:"dbd1";s:17:"locallang_tca.php";s:4:"22d1";s:15:"pagebrowser.gif";s:4:"d335";s:18:"pagebrowser__h.gif";s:4:"fbf4";s:25:"pagebrowser_notinmenu.gif";s:4:"fbf4";s:17:"pages_browser.gif";s:4:"10ca";s:20:"pages_browser__h.gif";s:4:"7928";s:14:"doc/manual.sxw";s:4:"134d";s:31:"pi1/class.tx_cagpagebrowser.php";s:4:"f432";s:20:"static/constants.txt";s:4:"872b";s:16:"static/setup.txt";s:4:"820b";s:30:"static/entrylink/constants.txt";s:4:"60d2";s:26:"static/entrylink/setup.txt";s:4:"d59b";s:32:"static/pagenumbers/constants.txt";s:4:"e53b";s:28:"static/pagenumbers/setup.txt";s:4:"7739";}',
+	'_md5_values_when_last_written' => 'a:24:{s:13:"CHANGELOG.txt";s:4:"2ec1";s:21:"ext_conf_template.txt";s:4:"30ee";s:12:"ext_icon.gif";s:4:"aea2";s:17:"ext_localconf.php";s:4:"a34a";s:14:"ext_tables.php";s:4:"4aaa";s:8:"TODO.txt";s:4:"d41d";s:14:"doc/manual.sxw";s:4:"76d8";s:21:"lang/locallang_db.xml";s:4:"c9e2";s:31:"pi1/class.tx_cagpagebrowser.php";s:4:"8a9f";s:19:"res/pagebrowser.gif";s:4:"d335";s:22:"res/pagebrowser__h.gif";s:4:"fbf4";s:29:"res/pagebrowser_notinmenu.gif";s:4:"fbf4";s:21:"res/pages_browser.gif";s:4:"10ca";s:24:"res/pages_browser__h.gif";s:4:"7928";s:37:"static/common/entrylink/constants.txt";s:4:"60d2";s:33:"static/common/entrylink/setup.txt";s:4:"d59b";s:23:"static/v1/constants.txt";s:4:"872b";s:19:"static/v1/setup.txt";s:4:"7adb";s:35:"static/v1/pagenumbers/constants.txt";s:4:"e53b";s:31:"static/v1/pagenumbers/setup.txt";s:4:"0894";s:23:"static/v2/constants.txt";s:4:"38ff";s:19:"static/v2/setup.txt";s:4:"0ded";s:36:"static/v2/treeprevnext/constants.txt";s:4:"5cf3";s:32:"static/v2/treeprevnext/setup.txt";s:4:"526c";}',
 	'suggests' => array(
 	),
 );
